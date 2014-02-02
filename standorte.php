d532d9afd290b0a7794fe668e3bcdb0fbd81fb7f
jaroslawvenzke <mail@jaroslawvenzke.de>
2014-02-02 09:20:57 +0100

    323

---
+++ standorte.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Century 21 - Standorte</title>
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+<style>
+HTML, BODY { 
+font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+	height: 	100%; 
+	margin: 	0px;
+	padding: 	0px;
+}
+
+h1 {
+	font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-size: 20px;
+	color: 		#000000;
+	font-weight: bold;
+}
+
+.fontBG { background-color:#f3b90e; padding:6px; margin:0px; }
+
+A { color: #ffffff; border: 0px; -moz-outline: none; }
+</style>
+</head>
+  
+<body>
+
+<table border="0" cellpadding="10">
+<tr>
+<td>
+
+<h1 class="fontBG">&Uuml;berschrift</h1>
+
+<p>Text</p>
+<p><?php echo 'IHR PHP CODE'; ?></p>
+
+<p>&nbsp;</p>
+
+</td>
+</tr>
+</table>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
